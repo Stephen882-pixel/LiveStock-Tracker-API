afde7a775ce2cf5c6214e6429a7d2501b374abd5
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-07-19 14:36:29 +0300

    BreedListCreateView

--- livestock_tracker/animals/views.py
+++ livestock_tracker/animals/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics,status
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from  rest_framework.filters import SearchFilter,OrderingFilter
+from .models import Animal,Breed
+from .serializers import (
+    AnimalListSerializer,
+    AnimalCreateSerializer,
+    AnimalDetailSerializer,
+    BreedSerializer
+)
 
 # Create your views here.
+class BreedListSerializer(generics.ListCreateAPIView):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['name','description']
+    ordering_fields = ["name","created_at"]
+    ordering =  ["name"]
+
